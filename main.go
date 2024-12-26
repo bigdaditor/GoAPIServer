a05ad24ed1c6110478e6b06a8eadaa4c4c02e32a
bigdaditor <bigdaditor1015@gmail.com>
2024-12-26 10:12:21 +0900

    1. Add Get Method - getBooks

--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-  "fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+// 책 자료
+type book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	ISBN   string `json:"isbn"`
+}
+
+var books = []book{
+	{ID: 1, Title: "Clean Code", Author: "James", ISBN: "971-80"},
+	{ID: 2, Title: "개발자 글쓰기", Author: "bigdaditor", ISBN: "971-81"},
+}
+
+func getBooks(book *gin.Context) {
+	book.IndentedJSON(http.StatusOK, books)
+}
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
-
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.Run(":8080")
 }
